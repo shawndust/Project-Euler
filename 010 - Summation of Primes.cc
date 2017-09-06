8ab4a8cf3944135a7d1096545d861ca2fa127038
Shawn Dust <shawndust.gh@gmail.com>
2017-09-06 16:12:04 -0500

    Update 010 - Summation of Primes.cc

--- 010 - Summation of Primes.cc
+++ 010 - Summation of Primes.cc
@@ -3,35 +3,51 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <math.h>
+#include <list>
+#include <set>
 using namespace std;
 
 void logic(int passNum)
 {
-	int sum = 0;
-	int tracker = 0;
-	for (int p = 2; p < passNum; p++)
-	{
-		tracker = 0;
-		for (int i = p - 1; i > 1; i--)
-		{
-			if (p % i == 0)
-				tracker += 1;
-		}
+	std::set<int> Aset;
+	cout << endl << "Creating List..." << endl;
 
-		if (tracker == 0)
+	for (int k = 0; k < passNum - 2; k++)
+	{
+		Aset.insert(k + 2);
+	}
+
+	cout << endl << "Pruning List..." << endl;
+	for (int i = 3; i < passNum; i++)
+	{
+		for (int t = 2; t < i; t++)
 		{
-			sum += p;
-	//		cout << endl << "Sum is " << sum << "\t" << p << endl;
+			if (i % t == 0)
+			{
+				if (Aset.count(i) > 0)
+				Aset.erase(i);
+			}
 		}
 	}
 
-	cout << endl << sum << endl;
+	cout << endl << "Summing List..." << endl;
+	int sum = 0;
+
+	for (std::set<int>::iterator it = Aset.begin(); it != Aset.end(); ++it)
+	{
+//		std::cout << ' ' << *it << endl;
+		sum += (int)* it;
+	}
+	cout << endl << "Sum is " << sum << endl;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int input;
-	cout << "Thinking for 30 min...";
+	int input = 0;
+	cout << flush;
+	cout << "Thinking..." << endl;
+	cout << flush;
 	logic(2000000);
 	cin >> input;
 	return 0;
