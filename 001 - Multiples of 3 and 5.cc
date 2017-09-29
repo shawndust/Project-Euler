75fbffc0d4d62e9dfd68dd4cb8428ad39eef9a7f
Shawn Dust <shawndust.gh@gmail.com>
2017-09-29 06:22:03 -0500

    Update 001 - Multiples of 3 and 5.cc

--- 001 - Multiples of 3 and 5.cc
+++ 001 - Multiples of 3 and 5.cc
@@ -1,3 +1,6 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
 #include "stdafx.h"
 #include "iostream"
 #include "iomanip"
@@ -7,7 +10,7 @@
 
 using namespace std;
 
-
+//Method takes topNum and returns all multiples of 3 or 5 less than it.
 int printer(int topNum)
 {
 	int sum = 0;
